c412c5f9711c4e7ac8ea7de813db7c803d82f090
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-09-08 23:01:57 +0800

    Update Rainbow.sh

--- Rainbow.sh
+++ Rainbow.sh
@@ -3,6 +3,13 @@
 # 脚本保存路径
 SCRIPT_PATH="$HOME/Rainbow.sh"
 
+# 检查是否以root用户运行脚本
+if [ "$(id -u)" != "0" ]; then
+    echo "此脚本需要以root用户权限运行。"
+    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
+    exit 1
+fi
+
 # 安装并启动节点的函数
 function install_and_start_node() {
     # 定义目录路径
